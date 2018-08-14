283b56d0b9bc19eaee0f534d8c0f590b9d5bd43e
chunyacu <31330067+chunyacu@users.noreply.github.com>
2018-08-14 11:18:03 +0800

    Rename to archdep-x86_64.mk

--- archdep-x86.mk
+++
@@ -1,9 +0,0 @@
-# bld/archdep-x86.mk, sub-make-file, target-arch dependent settings, for x86.
-# Written by Yingyu YOU (yingyu.you@hsmoptics.com), 2018-04-19
-# Copyright (c) 2018 Huaxin-SM Optics (Chengdu) Co., Ltd. All Rights Reserved.
-
-# Since x86 is used as host, cross-compiling is unnecessary.
-ifeq ($(VERBOSE),1)
-    $(info PATH=$(PATH))
-    $(info CROSS_COMPILE=$(CROSS_COMPILE))
-endif
