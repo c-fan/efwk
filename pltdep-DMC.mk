b956e40bf75ddb41bc6f7f984fbb3127ef9410fd
chunyacu <31330067+chunyacu@users.noreply.github.com>
2018-08-24 11:02:35 +0800

    Delete pltdep-DMC.mk

--- pltdep-DMC.mk
+++
@@ -1,3 +0,0 @@
-export GCC_HOME:=/usr/local/arm/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf
-export INC_GCC_CPP:=$(GCC_HOME)/arm-linux-gnueabihf/include/c++/6.2.1
-export INC_GCC_LIBC:=$(GCC_HOME)/arm-linux-gnueabihf/libc/usr/include
